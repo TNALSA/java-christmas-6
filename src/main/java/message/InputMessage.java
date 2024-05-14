a057b6f63d0f960cfb5360067a6eff711822ed3b
TNALSA <msa1859@naver.com>
2024-05-14 10:55:59 +0900

    feat: InputMessage 작성 및 OutputMessage 수정

--- src/main/java/message/InputMessage.java
+++ src/main/java/message/InputMessage.java
@@ -1,4 +1,13 @@
 package message;
 
 public enum InputMessage {
+    DATE_OF_VISIT("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    private String msg;
+    InputMessage(String msg){
+        this.msg = msg;
+    }
+    public String getMessage(){
+        return msg;
+    }
 }
--- src/main/java/message/OutputMessage.java
+++ src/main/java/message/OutputMessage.java
@@ -2,8 +2,6 @@ package message;
 
 public enum OutputMessage {
     WELCOME("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
-    DATE_OF_VISIT("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-    ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
     DECEMBER("12월"),
     PREVIEW("일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER_MENU("<주문 메뉴>"),
